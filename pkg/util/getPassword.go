d055be1f055d912ce1a8c82842c20a56c71e6733
Max <max.ott@data61.csiro.au>
2022-06-01 17:29:44 +1000

    another version of reading a password from the shell

---
+++ pkg/util/getPassword.go
@@ -0,0 +1,74 @@
+// License: MIT Open Source
+// Copyright (c) Joe Linoff 2016
+// Go code to prompt for password using only standard packages by utilizing syscall.ForkExec() and syscall.Wait4().
+// Correctly resets terminal echo after ^C interrupts.
+package util
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+)
+
+// techEcho() - turns terminal echo on or off.
+func termEcho(on bool) {
+	// Common settings and variables for both stty calls.
+	attrs := syscall.ProcAttr{
+		Dir:   "",
+		Env:   []string{},
+		Files: []uintptr{os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd()},
+		Sys:   nil}
+	var ws syscall.WaitStatus
+	cmd := "echo"
+	if on == false {
+		cmd = "-echo"
+	}
+
+	// Enable/disable echoing.
+	pid, err := syscall.ForkExec(
+		"/bin/stty",
+		[]string{"stty", cmd},
+		&attrs)
+	if err != nil {
+		panic(err)
+	}
+
+	// Wait for the stty process to complete.
+	_, err = syscall.Wait4(pid, &ws, 0, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// GetPassword - Prompt for password.
+func GetPassword(prompt string) string {
+	fmt.Print(prompt)
+
+	// Catch a ^C interrupt.
+	// Make sure that we reset term echo before exiting.
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, os.Interrupt)
+	go func() {
+		for _ = range signalChannel {
+			fmt.Println("\n^C interrupt.")
+			termEcho(true)
+			os.Exit(1)
+		}
+	}()
+
+	// Echo is disabled, now grab the data.
+	termEcho(false) // disable terminal echo
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+	termEcho(true) // always re-enable terminal echo
+	fmt.Println("")
+	if err != nil {
+		// The terminal has been reset, go ahead and exit.
+		fmt.Println("ERROR:", err.Error())
+		os.Exit(1)
+	}
+	return strings.TrimSpace(text)
+}
